f61921b8d66e62a801fa9ef03ade5dab7b311c03
DavidMaghradze <davidmaghradze07@gmail.com>
2018-06-28 14:50:23 +0400

    Added Catalog Section

--- index.php
+++ index.php
@@ -9,9 +9,47 @@
     <title></title>
 </head>
 <body>
-    <div class="container">
-        <h1>daad</h1>
-    </div>
+    <main>
+        <div class="container-fluid">
+            <header class="main-header">
+                <h2>კატალოგი</h2>
+            </header>
+            <div class="row no-gutters margin0">
+                <div class="col-md-6">
+                    <a href="#">
+                        <section class="catalog-item catalog-item-large">
+                            <img src="<?php bloginfo('template_url')?>/images/catalog-pic.jpg" alt="">
+                            <h3 class="catalog-item-header">რბილი ავეჯი</h3>
+                            <div class="catalog-inner"></div>
+                        </section>
+                    </a>
+                </div>
+                <div class="col-md-6">
+                    <a href="#">
+                        <section class="catalog-item catalog-item-medium">
+                            <img src="<?php bloginfo('template_url')?>/images/catalog-pic.jpg" alt="">
+                            <h3 class="catalog-item-header">რბილი ავეჯი</h3>
+                            <div class="catalog-inner"></div>
+                        </section>
+                    </a>
+                    <a href="#">
+                        <section class="catalog-item catalog-item-small">
+                            <img src="<?php bloginfo('template_url')?>/images/catalog-pic.jpg" alt="">
+                            <h3 class="catalog-item-header">რბილი ავეჯი</h3>
+                            <div class="catalog-inner"></div>
+                        </section>
+                    </a>
+                    <a href="#">
+                        <section class="catalog-item catalog-item-small">
+                            <img src="<?php bloginfo('template_url')?>/images/catalog-pic.jpg" alt="">
+                            <h3 class="catalog-item-header">რბილი ავეჯი</h3>
+                            <div class="catalog-inner"></div>
+                        </section>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </main>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
